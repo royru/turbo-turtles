2ee9202fcf8959c5049ffb0ca73a6ce280120525
royru <roy.rutishauser@gmail.com>
2023-12-07 15:03:30 +0100

    api/data boilerplate

---
+++ server/api/data.mjs
@@ -0,0 +1,15 @@
+// https://turbo-turtles.vercel.app/api/data
+
+export default function handler(
+  request,
+  response,
+) {
+
+  response.status(200).json({
+    users: [
+      { name: 'roy', steps: 5, color: 'red' },
+      { name: 'anastasia', steps: 10, color: 'blue' },
+      { name: 'sheena', steps: 15, color: 'green' }
+    ]
+  })
+}
