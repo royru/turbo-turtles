248078898e91e777745d7a7d9601d6be98a35ec5
SheenaGit <91433803+SheenaGit@users.noreply.github.com>
2023-12-07 12:33:44 +0100

    added kv db test files

---
+++ server/api/user.ts
@@ -0,0 +1,28 @@
+import { createClient } from '@vercel/kv'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+   request: NextApiRequest,
+   response: NextApiResponse
+) {
+   // Create a helper function to get an environment variable with a default value
+   const getEnvVar = (name: string, defaultValue: string): string => {
+      return process.env[name] || defaultValue
+   }
+
+   const users = createClient({
+      url: getEnvVar('USERS_REST_API_URL', ''),
+      token: getEnvVar('USERS_REST_API_TOKEN', '')
+   })
+
+   const user = await users.hgetall('user:me')
+
+   const products = createClient({
+      url: getEnvVar('PRODUCTS_REST_API_URL', ''),
+      token: getEnvVar('PRODUCTS_REST_API_TOKEN', '')
+   })
+
+   const product = await products.hgetall('product:shirt')
+
+   return response.status(200).json({ user, product })
+}
