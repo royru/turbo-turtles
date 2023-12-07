93c59cd49843fd5b73ff07df8d62149560be095a
royru <roy.rutishauser@gmail.com>
2023-12-07 16:18:42 +0100

    test

--- server/api/user.ts
+++ server/api/user.ts
@@ -4,23 +4,23 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 export default async function handler(request: VercelRequest, response: VercelResponse) {
   console.log('hiiii')
   // Create a helper function to get an environment variable with a default value
-  const getEnvVar = (name, defaultValue) => {
-    return process.env[name] || defaultValue
-  }
+  // const getEnvVar = (name, defaultValue) => {
+  //   return process.env[name] || defaultValue
+  // }
 
-  const users = createClient({
-    url: getEnvVar('USERS_REST_API_URL', ''),
-    token: getEnvVar('USERS_REST_API_TOKEN', '')
-  })
+  // const users = createClient({
+  //   url: getEnvVar('USERS_REST_API_URL', ''),
+  //   token: getEnvVar('USERS_REST_API_TOKEN', '')
+  // })
 
-  const user = await users.hgetall('user:me')
+  // const user = await users.hgetall('user:me')
 
-  const products = createClient({
-    url: getEnvVar('PRODUCTS_REST_API_URL', ''),
-    token: getEnvVar('PRODUCTS_REST_API_TOKEN', '')
-  })
+  // const products = createClient({
+  //   url: getEnvVar('PRODUCTS_REST_API_URL', ''),
+  //   token: getEnvVar('PRODUCTS_REST_API_TOKEN', '')
+  // })
 
-  const product = await products.hgetall('product:shirt')
+  // const product = await products.hgetall('product:shirt')
 
-  return response.status(200).json({ user, product })
+  return response.status(200).json({ name: "roy" })
 }
