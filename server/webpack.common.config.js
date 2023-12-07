e52c86feea1917c211548fdf6853ecabc5224f47
royru <roy.rutishauser@gmail.com>
2023-12-07 10:34:28 +0100

    test 4

--- server/webpack.common.config.js
+++ server/webpack.common.config.js
@@ -2,7 +2,6 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const Dotenv = require('dotenv-webpack')
 const webpack = require('webpack')
 
 module.exports = {
