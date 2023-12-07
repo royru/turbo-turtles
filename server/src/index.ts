3e3d0142649886ee22e32209e8053a10a32fea63
royru <roy.rutishauser@gmail.com>
2023-12-07 15:15:55 +0100

    data fetch setup not working

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,29 @@
-console.log("hello")
+console.log('hello')
+
+function render(data: any) {
+   const body = document.querySelector('body')
+   body.innerHTML = '' // this will clean the body whenever it is run
+
+   // TODO: Anastasia
+   console.log(data)
+}
+
+async function fetchAndRender() {
+   try {
+      const raw = await fetch('https://turbo-turtles.vercel.app/api/data', {
+         method: 'GET',
+         mode: 'no-cors'
+      })
+      console.log(raw)
+      const data = await raw.json()
+      render(data)
+   } catch (error) {
+      console.error(error)
+   }
+}
+
+async function main() {
+   setInterval(fetchAndRender, 1000)
+}
+
+main()
