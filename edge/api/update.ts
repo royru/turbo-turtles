0802948ff9dbea74bfecc2a002aef0a053c8b9f4
royru <roy.rutishauser@gmail.com>
2023-12-08 10:07:21 +0100

    test

--- edge/api/update.ts
+++ edge/api/update.ts
@@ -17,12 +17,12 @@ export default async function handler(request: VercelRequest, response: VercelRe
     let total_steps = total_steps_str ? Number.parseInt(total_steps_str) : 0
     console.log({ total_steps })
     if (total_steps == 0) {
-      const users_str = await kv.get('111users') as string | null
+      const users_str = await kv.get('users') as string | null
       console.log({ users_str })
       const users = users_str ? JSON.parse(users_str) : []
       users.push(user)
       console.log({ users })
-      await kv.sadd('111users', JSON.stringify(users))
+      await kv.sadd('users', JSON.stringify(users))
     }
     total_steps = total_steps + Number.parseInt(steps)
     await kv.set(user, total_steps.toString())
