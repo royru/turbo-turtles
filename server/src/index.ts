e8e5053c289d845bdd91be56fcb4f4e15b25f9d9
royru <roy.rutishauser@gmail.com>
2023-12-08 10:30:52 +0100

    test

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,156 +2,156 @@ var render_count = 0
 var raceTrackWidth = 1000
 
 var total_steps_per_user: { [key: number]: number } = {
-   1: 0,
-   2: 0,
-   3: 0,
-   4: 0,
-   5: 0
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0
 }
 
 var color_defs = {
-   green: {
-      path_color: '#F2FBB9'
-   },
-   blue: {
-      path_color: '#E6F5FF'
-   },
-   purple: {
-      path_color: '#EED5F9'
-   },
-   pink: {
-      path_color: '#FDD8E4'
-   },
-   orange: {
-      path_color: '#FFE2B4'
-   }
+  green: {
+    path_color: '#F2FBB9'
+  },
+  blue: {
+    path_color: '#E6F5FF'
+  },
+  purple: {
+    path_color: '#EED5F9'
+  },
+  pink: {
+    path_color: '#FDD8E4'
+  },
+  orange: {
+    path_color: '#FFE2B4'
+  }
 }
 
 function getMaxScore() {
-   let maxScore = 0
+  let maxScore = 0
 
-   for (let userId in total_steps_per_user) {
-      if (total_steps_per_user.hasOwnProperty(userId)) {
-         maxScore = Math.max(maxScore, total_steps_per_user[userId])
-      }
-   }
-   return maxScore
+  for (let userId in total_steps_per_user) {
+    if (total_steps_per_user.hasOwnProperty(userId)) {
+      maxScore = Math.max(maxScore, total_steps_per_user[userId])
+    }
+  }
+  return maxScore
 }
 
 console.log('hello')
 
 function render(data: any) {
-   const body = document.querySelector('body')
-   body.style.fontFamily = 'Trebuchet MS'
-   body.innerHTML = '' // this will clean the body whenever it is run
+  const body = document.querySelector('body')
+  body.style.fontFamily = 'Trebuchet MS'
+  body.innerHTML = '' // this will clean the body whenever it is run
 
-   console.log(data)
-   const title = document.createElement('h1')
-   title.textContent = `Turtle Race! ${render_count}`
-   body.appendChild(title)
+  console.log(data)
+  const title = document.createElement('h1')
+  title.textContent = `Turtle Race! ${render_count}`
+  body.appendChild(title)
 
-   for (let user of data) {
-      const user_name = user.name
-      const user_color: 'green' | 'blue' | 'purple' | 'pink' | 'orange' =
-         user.color
-      const user_id: 1 | 2 | 3 | 4 | 5 = user.id
-      // const user_steps = total_steps_per_user[user_id]
-      const user_steps = user.steps
-      const user_steps_px = `${user_steps * 1}px`
-      const user_path_color = color_defs[user_color].path_color
-      const left_or_right: string = render_count % 2 === 0 ? '0' : '1'
-      const turtle_img_path = `assets/${user_color}_turtle_${left_or_right}.svg`
+  for (let user of data) {
+    const user_name = user.name
+    const user_color: 'green' | 'blue' | 'purple' | 'pink' | 'orange' =
+      user.color
+    const user_id: 1 | 2 | 3 | 4 | 5 = user.id
+    // const user_steps = total_steps_per_user[user_id]
+    const user_steps = user.steps
+    const user_steps_px = `${user_steps * 1}px`
+    const user_path_color = color_defs[user_color].path_color
+    const left_or_right: string = render_count % 2 === 0 ? '0' : '1'
+    const turtle_img_path = `assets/${user_color}_turtle_${left_or_right}.svg`
 
-      // .race-track
-      const race = document.createElement('div')
-      race.className = 'race-track'
-      race.style.width = `${raceTrackWidth}px`
-      body.appendChild(race)
+    // .race-track
+    const race = document.createElement('div')
+    race.className = 'race-track'
+    race.style.width = `${raceTrackWidth}px`
+    body.appendChild(race)
 
-      // .progress-bar
-      const progress = document.createElement('div')
-      progress.className = 'progress-bar'
-      progress.style.width = '100%'
-      progress.style.backgroundColor = '#e0e0e0'
-      progress.style.borderRadius = '5px'
-      progress.style.overflow = 'hidden'
-      progress.style.position = 'relative'
-      race.appendChild(progress)
+    // .progress-bar
+    const progress = document.createElement('div')
+    progress.className = 'progress-bar'
+    progress.style.width = '100%'
+    progress.style.backgroundColor = '#e0e0e0'
+    progress.style.borderRadius = '5px'
+    progress.style.overflow = 'hidden'
+    progress.style.position = 'relative'
+    race.appendChild(progress)
 
-      // .progress
-      const progressInner = document.createElement('div')
-      progressInner.className = 'progress'
-      progressInner.style.width = user_steps_px
-      progressInner.style.height = '40px'
-      progressInner.style.borderRadius = '5x'
-      progressInner.style.position = 'relative'
-      progressInner.style.color = '#2f2f2f'
-      progressInner.style.lineHeight = '20px'
-      progressInner.style.textAlign = 'left'
-      progressInner.style.paddingLeft = '5px'
-      progressInner.style.backgroundColor = user_path_color
-      progressInner.textContent = user_name
-      progress.appendChild(progressInner)
+    // .progress
+    const progressInner = document.createElement('div')
+    progressInner.className = 'progress'
+    progressInner.style.width = user_steps_px
+    progressInner.style.height = '40px'
+    progressInner.style.borderRadius = '5x'
+    progressInner.style.position = 'relative'
+    progressInner.style.color = '#2f2f2f'
+    progressInner.style.lineHeight = '20px'
+    progressInner.style.textAlign = 'left'
+    progressInner.style.paddingLeft = '5px'
+    progressInner.style.backgroundColor = user_path_color
+    progressInner.textContent = user_name
+    progress.appendChild(progressInner)
 
-      const verticalLine = document.createElement('div')
-      verticalLine.className = 'vertical-line'
-      verticalLine.style.width = '2px'
-      verticalLine.style.height = '100%'
-      verticalLine.style.backgroundColor = 'black'
-      verticalLine.style.position = 'absolute'
-      verticalLine.style.right = '0'
-      progress.appendChild(verticalLine)
+    const verticalLine = document.createElement('div')
+    verticalLine.className = 'vertical-line'
+    verticalLine.style.width = '2px'
+    verticalLine.style.height = '100%'
+    verticalLine.style.backgroundColor = 'black'
+    verticalLine.style.position = 'absolute'
+    verticalLine.style.right = '0'
+    progress.appendChild(verticalLine)
 
-      const imgTurtle = document.createElement('img')
-      imgTurtle.className = 'img.turtle'
-      imgTurtle.style.height = '40px'
-      imgTurtle.style.left = user_steps_px
-      imgTurtle.style.position = 'absolute'
-      imgTurtle.style.top = '0'
-      imgTurtle.style.transform = 'translateX(-50%)'
-      imgTurtle.src = turtle_img_path
-      progress.appendChild(imgTurtle)
-   }
+    const imgTurtle = document.createElement('img')
+    imgTurtle.className = 'img.turtle'
+    imgTurtle.style.height = '40px'
+    imgTurtle.style.left = user_steps_px
+    imgTurtle.style.position = 'absolute'
+    imgTurtle.style.top = '0'
+    imgTurtle.style.transform = 'translateX(-50%)'
+    imgTurtle.src = turtle_img_path
+    progress.appendChild(imgTurtle)
+  }
 }
 
 async function fetchAndRender() {
-   try {
-      render_count += 1
+  try {
+    render_count += 1
 
-      // for (let userId in total_steps_per_user) {
-      //    if (total_steps_per_user.hasOwnProperty(userId)) {
-      //       // Use bracket notation with a type assertion
-      //       total_steps_per_user[parseInt(userId)] += Math.floor(
-      //          Math.random() * 6
-      //       )
-      //    }
-      // }
+    // for (let userId in total_steps_per_user) {
+    //    if (total_steps_per_user.hasOwnProperty(userId)) {
+    //       // Use bracket notation with a type assertion
+    //       total_steps_per_user[parseInt(userId)] += Math.floor(
+    //          Math.random() * 6
+    //       )
+    //    }
+    // }
 
-      const raw = await fetch(
-         'https://turbo-turtles-edge.vercel.app/api/data',
-         {
-            method: 'GET',
-            mode: 'no-cors'
-         }
-      )
-      console.log(raw)
-      const data = await raw.json()
+    const raw = await fetch(
+      'https://turbo-turtles-edge.vercel.app/api/data',
+      {
+        method: 'GET',
+        mode: 'no-cors'
+      }
+    )
+    const data = await raw.json()
+    console.log(data)
 
-      // const data = [
-      //    { id: 1, name: 'Roy', steps: 5, color: 'green' },
-      //    { id: 2, name: 'Anastasia', steps: 10, color: 'blue' },
-      //    { id: 3, name: 'Sheena', steps: 15, color: 'purple' },
-      //    { id: 4, name: 'Tarek', steps: 20, color: 'pink' },
-      //    { id: 5, name: 'Sebastian', steps: 25, color: 'orange' }
-      // ]
-      render(data)
-   } catch (error) {
-      console.error(error)
-   }
+    // const data = [
+    //    { id: 1, name: 'Roy', steps: 5, color: 'green' },
+    //    { id: 2, name: 'Anastasia', steps: 10, color: 'blue' },
+    //    { id: 3, name: 'Sheena', steps: 15, color: 'purple' },
+    //    { id: 4, name: 'Tarek', steps: 20, color: 'pink' },
+    //    { id: 5, name: 'Sebastian', steps: 25, color: 'orange' }
+    // ]
+    render(data)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 async function main() {
-   setInterval(fetchAndRender, 500)
+  setInterval(fetchAndRender, 500)
 }
 
 main()
